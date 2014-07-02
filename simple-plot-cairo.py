ade830d2ee560fe5818068714e8a21392dbb71e6
Jochen Voss <voss@seehuhn.de>
2014-07-02 18:11:52 +0100

    Clean up the simple-plot-cairo.py script some more.
    This commit introduces the following changes:
    - change the paper format to A4 for more convenient printing
    - get the scaling right
    - iterate over the shapes more efficiently
    - remove some unused and unnecessary code
    - make it clear that this script can be redistributed (under the
      terms of the GPL)

--- simple-plot-cairo.py
+++ simple-plot-cairo.py
@@ -1,55 +1,65 @@
 #! /usr/bin/env python
+#
+# Plot all shapes contained in the "leedsAll" shapefile.
+# Copyright (C) 2014 Jochen Voss <voss@seehuhn.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
 import cairo
 import math
 import shapefile
 
-sf = shapefile.Reader("pythonGeoHack/leedsAll")
-shapes = sf.shapes()
-
-width = 8.0 * 72
-height = 8.0 * 72
-surface = cairo.PDFSurface("map.pdf", width, height)
+cm = 72 / 2.54
+width = 29.7 * cm
+height = 21 * cm
+margin = .5 * cm
+surface = cairo.PDFSurface("simple-plot.pdf", width, height)
 ctx = cairo.Context(surface)
-
-lon_min, lat_min, lon_max, lat_max = sf.bbox
-
-lon_mid = (lon_min + lon_max) / 2
-lat_mid = (lat_min + lat_max) / 2
-r = 6371
-x_range = (lat_max - lat_min) / 360.0 * 2 * math.pi * r * math.cos(lat_mid * 2 * math.pi / 360.0)
-y_range = (lon_max - lon_min) / 360.0 * 2 * math.pi * r
-scale = min(width / x_range, height / y_range)
-
-# move the origin to the center
+# move the origin to the center:
 ctx.scale(1, -1)
 ctx.translate(.5*width, -.5*height)
 
-ctx.set_line_width(0.1)
+sf = shapefile.Reader("pythonGeoHack/leedsAll")
+lon_min, lat_min, lon_max, lat_max = sf.bbox
+lon_mid = (lon_min + lon_max) / 2
+lat_mid = (lat_min + lat_max) / 2
+lat_corr = math.cos(lat_mid * 2 * math.pi / 360.0)
 
-qx = 1500 * math.cos(lat_mid * 2 * math.pi / 360.0)
-qy = 1500
+qx = (width - 2*margin) / (lon_max - lon_min) / lat_corr
+qy = (height - 2*margin) / (lat_max - lat_min)
+q = min(qx, qy)
+
+ctx.set_line_width(0.15)
 
 edge_count = 0
-node_count = 0
-for s in shapes:
+for s in sf.iterShapes():
     if tuple(s.parts) != (0,):
         raise NotImplementedError("s.parts = %s" % s.parts)
     if s.shapeType != 3:
+        # Shape types are explaine on page 4 of
+        # http://www.esri.com/library/whitepapers/pdfs/shapefile.pdf ,
+        # type 3 means "PolyLine".
         raise NotImplementedError("s.shapetype = %s" % s.shapeType)
     if len(s.points) < 2:
         raise NotImplementedError("degenerate shape with only one point")
 
-    node_count += len(s.points)
     edge_count += len(s.points) - 1
-    for j, x in enumerate(s.points):
+    for j, z in enumerate(s.points):
+        x = (z[0] - lon_mid) * q * lat_corr
+        y = (z[1] - lat_mid) * q
         if j == 0:
-            ctx.move_to((x[0] - lon_mid)*qx, (x[1]-lat_mid)*qy)
+            ctx.move_to(x, y)
         else:
-            ctx.line_to((x[0] - lon_mid)*qx, (x[1]-lat_mid)*qy)
+            ctx.line_to(x, y)
     ctx.stroke()
+print(edge_count, "line segments")
 
 surface.finish()
-
-print(node_count, "nodes")
-print(edge_count, "edges")
